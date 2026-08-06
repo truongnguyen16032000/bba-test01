531215e67e956b2e5c5bf18442b3566ab8c6e1d0
test 01 - Nguyện Trương <truongnguyen16032000@gmail.com>
2026-08-07 01:22:44 +0900

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,17 @@
+let powerUp ="mushroom";
+if (powerUp === "mushroom") {
+  console.log("Mario becomes Super!");
+}
+else if (powerUp === "flower") {
+  console.log("Mario can shoot fireballs!");
+}
+else if (powerUp ==="star") {
+  console.log("Mario is invincible!");
+}
+else if(powerUp === "None") {
+    console.log("Mario is normal.");
+}
+else{
+    console.log("Unknown power-up!");
+}
+//Ban dau em dung if het nhung chay ra 2 ket qua nen em dung if else nhu nay de chi lay 1 ket qua duy nhat thoi a
